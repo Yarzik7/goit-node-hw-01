6ec7cebafc573bd7c0fae71b208676893edad6b3
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-08-01 22:12:06 +0300

    upd

--- contacts.js
+++ contacts.js
@@ -1,19 +1,30 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const contactsPath = null;
+const contactsPath = path.join(__dirname, 'db/contacts.json');
+console.log(contactsPath);
 
 // TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
+async function listContacts() {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
 }
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contactById = contacts.find(({ id }) => id === contactId);
+    return contactById ?? null;
 }
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+async function removeContact(contactId) {
+   const contacts = await listContacts();
+   const contactIdx = contacts.findIndex(({ id }) => id === contactId)
+    if (contactIdx === -1) {
+        return null;
+    }
+    const [contact] = contacts.splice(contactIdx, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contact;
 }
 
 function addContact(name, email, phone) {
